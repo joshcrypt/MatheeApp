e7685abd155685f72295bfb3524d2c42b73690ea
kenny <kennedykinyanjui@outlook.com>
2019-05-21 19:22:24 +0300

    house keeping/refactoring

--- ui/test.js
+++
@@ -1,9 +0,0 @@
-var where = require("lodash.where");
-
-var json = '[{"user": "a", "age": 20}, {"user": "b", "age": 30}, {"user": "c", "age": 40}]';
-
-var users = JSON.parse(json);
-
-var filtered = where(users, {"user": "a"});
-
-console.log(filtered);
